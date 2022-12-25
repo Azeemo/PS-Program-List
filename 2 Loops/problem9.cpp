0eb06ec356e46f1d40ccf82e0806e21f9942efe4
Azeem Khan <98760240+Azeemo@users.noreply.github.com>
2022-12-25 16:28:43 +0530

    Add files via upload

---
+++ 2 Loops/problem9.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+
+
+{
+
+int n;
+cin>>n;
+
+int k = n/2;
+for(int i=1;i<=k;i++)
+{
+   	for(int j=0;j<=k-i;j++)
+   	{
+   		cout<<" ";
+   	}
+   	  		
+   	 for(int j=1;j<=2*i-1;j++)
+   	{    
+   		cout<<"*";
+   	  
+   	  		}
+   	  		cout<<endl;
+   	  	}
+
+    int f = n-k;
+    	 
+    	 for(int i=0;i<f;i++)
+    	 {
+    	    	for(int j=-0;j<=i-1;j++)
+    	    	{ 
+    	    		cout<<" ";
+    	    	}
+    	    	  		
+    	    	  		for(int j=1;j<=2*f-2*i-1;j++)
+    	    	  		{
+    	    	  		   	cout<<"*";
+    	    	  		}
+    	    	  		
+    	  cout<<endl;  	  		
+    	 }
+    	 
+    	 return 0 ;
+
+    	  }
+    	  
+    	  	  
+   
+
+
+
+
+
+
+
+
